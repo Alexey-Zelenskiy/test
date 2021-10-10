e05eb2371291bda1a465999d66d3aff95b7a8c42
Azelenskiy <zelenskii1999@mail.ru>
2021-10-11 00:59:39 +0700

    initial

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-proposal-decorators', {legacy: true}],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '~': './src',
+          },
+        },
+      ],
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+import {LogBox} from 'react-native';
 import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
+import {registerRootComponent} from 'expo';
 
-import App from './App';
+import App from './src/app';
+
+LogBox.ignoreLogs([
+  'Warning: componentWillReceiveProps',
+  'Warning: Require cycle',
+]);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
